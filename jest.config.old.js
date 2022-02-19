d1ced8b87f12590764a378cdd5b0485dfb642786
Mark Vasile <codeawareness@gmail.com>
2022-02-19 01:40:46 -0800

    fixed build

--- jest.config.old.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleDirectories: ['node_modules', 'src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-}
